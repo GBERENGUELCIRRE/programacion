d4630d99b116f1ec9733542b97c054c7d1dc1944
guillermoberenguel <guillermoberenguel@pc208-8.04700144.04.andared.ced.junta-andalucia.es>
2018-10-10 18:57:07 +0200

    porel

---
+++ GBCirre-Pre02/src/GBCirre/Ejercicio02.java
@@ -0,0 +1,21 @@
+
+package GBCirre;
+import java.util.Scanner;
+
+public class Ejercicio02 {
+    public static void main (String [] args){
+        int numero=0;
+        int division=0;
+        Scanner sc = new Scanner (System.in);
+        System.out.println("Introducir numero");
+        numero = sc.nextInt();
+        division = numero/2;
+        
+        if {
+            
+        }
+        
+        
+        
+}
+}
