15fb3aa7e73e0185f7c3c2a4c6076f40d92b088c
guillermoberenguel <guillermoberenguel@pc208-8.04700144.04.andared.ced.junta-andalucia.es>
2018-10-10 20:41:58 +0200

    porel

---
+++ GBCirre-Pre02/src/GBCirre/Ejercicio03.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package GBCirre;
+
+import java.util.Scanner;
+public class Ejercicio03 {
+    public static void main (String [] args){
+        int numero=0;
+        Scanner sc = new Scanner (System.in);
+        System.out.println("Introducir numero de mes");
+        numero = sc.nextInt();
+        switch (numero);
+                {
+            
+        }
+        
+    }
+    
+}
