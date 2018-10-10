4e78ab38b890af945f225f692f3d39ae30dbbe35
guillermoberenguel <guillermoberenguel@pc208-8.04700144.04.andared.ced.junta-andalucia.es>
2018-10-10 20:42:12 +0200

    porel

--- GBCirre-Pre02/src/GBCirre/Ejercicio02.java
+++ GBCirre-Pre02/src/GBCirre/Ejercicio02.java
@@ -5,16 +5,16 @@ import java.util.Scanner;
 public class Ejercicio02 {
     public static void main (String [] args){
         int numero=0;
-        int division=0;
         Scanner sc = new Scanner (System.in);
         System.out.println("Introducir numero");
         numero = sc.nextInt();
-        division = numero/2;
-        
-        if {
+        if (numero%2==0) {
+            System.out.println("EL numero es par");
             
         }
-        
+        else{
+            System.out.println("El numero es impar");
+        }
         
         
 }
