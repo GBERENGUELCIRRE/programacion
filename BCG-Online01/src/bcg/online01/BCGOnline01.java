a9f845fa457a43b72e1a04e7cfc2dddd361ed250
guillermoberenguel <guillermoberenguel@pc208-8.04700144.04.andared.ced.junta-andalucia.es>
2020-10-04 13:33:24 +0200

    EJERCICIOS ONLINE 4-10-2020

---
+++ BCG-Online01/src/bcg/online01/BCGOnline01.java
@@ -0,0 +1,21 @@
+
+package bcg.online01;
+
+/**
+ *
+ * @author guillermoberenguel
+ */
+import java.util.Scanner;
+
+public class BCGOnline01 {
+
+   
+    public static void main(String[] args) {
+        // PARA CREAR UNA VARIABLE ENTERA PONEMOS EL TIPO INT, CON EL NOMBRE
+        // Y SI QUEREMOS LE DAMOS UN VALOR, POR EJEMPLO EL 5
+        int numero=5;
+        // AHORA MOSTRAMOS POR PANTALLA EL NOMBRE
+        System.out.println("GUILLERMO BERENGUEL CIRRE");
+    }
+    
+}
